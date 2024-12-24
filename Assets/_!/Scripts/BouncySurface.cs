d587283e201f249783dd17c04020c9b139decd96
Varol Çivril <93577667+Necrilimoglsed@users.noreply.github.com>
2024-12-24 18:43:49 +0300

    Initial commit

---
+++ Assets/_!/Scripts/BouncySurface.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BouncySurface : MonoBehaviour
+{
+    public float bounceStrength;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball ball = collision.gameObject.GetComponent<Ball>();
+
+        if (ball != null)
+        {
+            Vector2 normal = collision.GetContact(0).normal;
+            ball.AddForce(-normal * this.bounceStrength);
+        }
+    }
+}
